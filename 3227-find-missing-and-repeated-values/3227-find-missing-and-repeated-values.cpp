6f2fa89141613b45328b3d2aa84b3e92b5a833bd
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2025-07-31 14:36:49 +0530

    Update 3227-find-missing-and-repeated-values.cpp

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         
-        int n = grid.size();
-        int size = n * n;
-        vector<int> freq(size + 1, 0); // freq[0] unused
+        int n=grid.size();
+        int size=n*n;
+        vector<int> freq(size+1,0);
 
-        // Count frequencies
-        for (auto& row : grid) {
-            for (int num : row) {
+        for (auto& row:grid){
+            for(int num:row){
                 freq[num]++;
             }
         }
 
-        int repeated = -1, missing = -1;
-        for (int i = 1; i <= size; i++) {
-            if (freq[i] == 2) repeated = i;
-            if (freq[i] == 0) missing = i;
+        int repeated=-1, missing=-1;
+        for(int i=1;i<=size;i++){
+            if(freq[i]==2) repeated=i;
+            if(freq[i]==0) missing=i;
         }
 
-        return {repeated, missing};
-    
+        return {repeated,missing};
     }
 };
