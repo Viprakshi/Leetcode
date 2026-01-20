b6677edab2bfcdc22d3c86d8ae6efd50fa2a511e
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-01-20 11:49:56 +0530

    Time: 0 ms (100%), Space: 25.1 MB (78.7%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                ans.push_back(-1);
+            } 
+            else{
+                int best = -1;
+            for (int x = 0; x <= n; x++) {
+                if ((x | (x + 1)) == n) {
+                    best = x;
+                    break; 
+                }
+            }
+            ans.push_back(best);
+            }
+            
+        }
+        
+        return ans;
+    }
+};
