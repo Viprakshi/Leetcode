a71ed31c26f2c294a9c7313e84a92f0039064745
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-01-21 22:51:16 +0530

    Time: 1 ms (32.6%), Space: 77.3 MB (18.78%) - LeetHub

---
+++ 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int isWinner(vector<int>& player1, vector<int>& player2) {
+            int player_1=player1[0], player_2=player2[0];
+            for(int i=1;i<player1.size();i++){
+                if(i==1 && player1[i-1]==10) player_1+=2*player1[i];
+                else if(i>=2 && (player1[i-1]==10 || player1[i-2]==10)) player_1+=2*player1[i];
+                else player_1+=player1[i];
+            }
+            for(int i=1;i<player2.size();i++){
+                if(i==1 && player2[i-1]==10) player_2+=2*player2[i];
+                else if(i>=2 && (player2[i-1]==10 || player2[i-2]==10)) player_2+=2*player2[i];
+                else player_2+=player2[i];
+            }
+            if(player_1==player_2) return 0;
+            else if(player_2>player_1) return 2;
+            else return 1;
+
+    }
+};
