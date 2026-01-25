f470809c3232fe8f148d271b14a5645f5e8dc23e
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-01-25 21:16:30 +0530

    Time: 4 ms (23.72%), Space: 17.3 MB (56.92%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -4,12 +4,8 @@ public:
         sort(nums.begin(),nums.end());
         int sum=INT_MAX;
         for(int i=0;i<=nums.size()-k;i++){
-            int left=i, right=i+k-1;
-            while(right<nums.size()){
-                sum=min(sum,nums[right]-nums[left]);
-                left++;
-                right++;
-            }
+            int left=i, right=i+k-1; 
+            sum=min(sum,nums[right]-nums[left]); 
         }   
         return sum;
     }
