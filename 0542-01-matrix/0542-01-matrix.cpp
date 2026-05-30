844b451dca575870531b8cf8f0e0cddeddaaab90
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-05-31 00:21:53 +0530

    Time: 14 ms (74.07%), Space: 38.9 MB (29.8%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size(), n=mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<pair<int,int>, int>> q;
+
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=1;
+                }
+            }
+        }
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n; j++){
+                if(vis[i][j]==1){
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        vector<int> deltarow={-1,0,1,0};
+        vector<int> deltacol={0,1,0,-1};
+        while(!q.empty()){
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int z=q.front().second;
+            q.pop();
+            dist[x][y]=min(z,dist[x][y]);
+            
+            for(int i=0;i<4;i++){
+                int nr=x+deltarow[i];
+                int nc=y+deltacol[i];
+                if(nr<0 || nc<0 || nr>=m || nc>=n) continue;
+                if(!vis[nr][nc])
+                {
+                    vis[nr][nc]=1;
+                    q.push({{nr,nc},z+1});
+                }
+
+            }
+
+        }
+        return dist;
+
+    }
+};
