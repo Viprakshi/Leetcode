5cd252117104a4af176bed965711c56c3bcf76cc
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-05-31 17:42:35 +0530

    Time: 35 ms (13.87%), Space: 80.3 MB (77.63%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -3,10 +3,10 @@ public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         sort(players.begin(), players.end());
         sort(trainers.begin(), trainers.end());
-        int left=0, right=0, ans=0;
+        int left=0, right=0;
         while(left<players.size() && right<trainers.size()){
             if(players[left]<=trainers[right]){
-                ans++;
+                
                 left++;
             }
             right++;
