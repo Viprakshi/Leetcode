1f994a90a5b87fdfef8573f70d4d8644a8531ab9
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-05-31 18:38:39 +0530

    Time: 0 ms (100%), Space: 87.2 MB (43.42%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int> change(3,0);
+        vector<int> change(2,0);
         for(int i=0;i<bills.size();i++){
             if(bills[i]==5)
             {
@@ -14,14 +14,14 @@ public:
             }        
             if(bills[i]==20){
                 if (change[0] > 0 && change[1] > 0) {
-                    change[0]--; // Use one $5
-                    change[1]--;  // Use one $10
+                    change[0]--; 
+                    change[1]--;  
                 } 
                 else if (change[0] >= 3) {
-                    change[0] -= 3; // Use three $5 bills
+                    change[0] -= 3; 
                 } 
                 else {
-                    return false; // Cannot provide change
+                    return false; 
                 }
             }
         }
