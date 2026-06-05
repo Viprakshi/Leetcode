71059398c4cc09226011f943fbd03ac0cecca072
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-05 17:24:00 +0530

    Time: 6 ms (22.57%), Space: 9.6 MB (91.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        unordered_map<char, int> s_map;
-        unordered_map<char, int> t_map;
-        for(int i=0;i<s.length();i++){
-            s_map[s[i]]++;
-            t_map[t[i]]++;
-        }
-        for(int i=0;i<s.length();i++){
-            if(s_map[s[i]]!=t_map[s[i]]) return false;
-        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s!=t) return false;
         return true;
     }
 };
