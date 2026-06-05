834a10b63f031b01425e62c1781ddeb01f0b90f5
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-05 15:41:18 +0530

    Time: 84 ms (54.61%), Space: 43.9 MB (93.18%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n+1];
-        for(auto time:times){
-            adj[time[0]].push_back({time[1],time[2]});
+
+        vector<pair<int,int>> adj[n + 1];
+
+        for(auto &t : times){
+            adj[t[0]].push_back({t[1], t[2]});
         }
 
-        vector<int> time(n+1,INT_MAX);
-        queue<pair<int,int>> q;
-        time[k]=0;
-        q.push({k,0});
-        int ans=0;
-        while(!q.empty()){
-            int node=q.front().first;
-            int time_node=q.front().second;
-            q.pop();
-            for(auto adj_node:adj[node]){
-                int node_adj=adj_node.first;
-                int time_adj=adj_node.second;
-                if(time_node+time_adj<time[node_adj]){
-                    time[node_adj]=time_node+time_adj;
-                    q.push({node_adj, time_node+time_adj});
+        vector<int> dist(n + 1, INT_MAX);
+
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
+
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+
+            auto [time, node] = pq.top();
+            pq.pop();
+
+            for(auto &[adjNode, wt] : adj[node]){
+
+                if(time + wt < dist[adjNode]){
+                    dist[adjNode] = time + wt;
+                    pq.push({dist[adjNode], adjNode});
                 }
-                
             }
         }
 
-for(int i=1;i<=n;i++){
-    if(time[i]==INT_MAX) return -1;
-    ans = max(ans, time[i]);
-}
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(dist[i] == INT_MAX) return -1;
+            ans = max(ans, dist[i]);
+        }
+
         return ans;
     }
 };
