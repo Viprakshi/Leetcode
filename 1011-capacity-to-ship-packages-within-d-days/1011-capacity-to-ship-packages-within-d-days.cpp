10cd8910e800c489a2c014916b2e82be9391616e
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-13 12:17:00 +0530

    Time: 15 ms (32.43%), Space: 35.1 MB (52.83%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int daysNeeded(vector<int>& w, int cap) {
-        int days = 1, load = 0;
-        for (int x : w) {
-            if (load + x > cap) {
+    int count_days(int weight_given, vector<int>& weights){
+        int days=1;
+        int x=0;
+        for(int weight:weights){
+            if(x+weight<=weight_given)
+            x+=weight;
+            else{
                 days++;
-                load = x;
-            } else {
-                load += x;
+                x=weight;
             }
         }
         return days;
     }
-
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        int ans = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (daysNeeded(weights, mid) <= days) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+               
+        int left=*max_element(weights.begin(),weights.end()), right=accumulate(weights.begin(),weights.end(),0);
+        int ans=right;
+        while(left<=right){
+            int mid=(left+right)/2;
+            int days_required=count_days(mid, weights);
+            if(days_required<=days){
+                ans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
         return ans;
     }
 };
-
