5244d71347ba541814417def3656ab464d21b328
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-18 11:42:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (20.25%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<=sqrt(num);i++){
+            if(num%i==0){
+                sum+=i;
+                if(i!=num/i) sum+=(num/i);
+            }
+        }
+        sum-=num;
+        if(sum==num) return true;
+        return false;
+    }
+};
