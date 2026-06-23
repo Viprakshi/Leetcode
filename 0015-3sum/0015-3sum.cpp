4fe96cb40016b07e7626afc0def314228a6ea18a
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-23 13:15:28 +0530

    Time: 43 ms (83.99%), Space: 29.3 MB (30.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        int n=arr.size();
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-    sort(arr.begin(), arr.end());
-    for (int i = 0; i < n; i++) {
-        //remove duplicates:
-        if (i != 0 && arr[i] == arr[i - 1]) continue;
-
-        //moving 2 pointers:
-        int j = i + 1;
-        int k = n - 1;
-        while (j < k) {
-            int sum = arr[i] + arr[j] + arr[k];
-            if (sum < 0) {
-                j++;
-            }
-            else if (sum > 0) {
-                k--;
-            }
-            else {
-                vector<int> temp = {arr[i], arr[j], arr[k]};
-                ans.push_back(temp);
-                j++;
-                k--;
-                //skip the duplicates:
-                while (j < k && arr[j] == arr[j - 1]) j++;
-                while (j < k && arr[k] == arr[k + 1]) k--;
+        for(int i=0;i<nums.size()-1;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1, k=nums.size()-1;
+            while(j<k){
+                
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j>0 && j<nums.size() && nums[j]==nums[j-1]) j++;
+                    while(k<nums.size()-1 && nums[k]==nums[k+1]) k--;
+                } 
+                else if(sum<0) j++;
+                else k--;
             }
         }
-    }
-    return ans;
-
+        return ans;
     }
 };
