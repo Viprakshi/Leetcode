e67017294e95ceddd85bce58215872a2b54da723
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-06-26 10:43:36 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int cal(vector<int>& nums,int curr, vector<int>& dp ){
-        if(curr<0) return 0;
         if(curr==0) return nums[0];
-        if(dp[curr]!=-1) return dp[curr];
-    
-        int include=nums[curr]+cal(nums, curr-2,dp);
-        int not_include=cal(nums,curr-1,dp);
-        dp[curr]=max(include, not_include);
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++){
+            int inc=dp[i-2]+nums[i];
+            int ninc=dp[i-1];
+            dp[i]=max(inc, ninc);
+        }
         return dp[curr];
     }
     int rob(vector<int>& nums) {
