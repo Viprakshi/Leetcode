a1c114bbaa65362053e3aacb1c8ed0e740749ecd
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-02 17:34:17 +0530

    Time: 0 ms (100%), Space: 12.7 MB (39.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,16 +5,13 @@ public:
         if(nums.size()==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
-        int left=1,  right=n-2;
+        int left=1, right=n-2;
         while(left<=right){
-            int mid=(left+right)/2;
+            int mid=left+(right-left)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid+1]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
+            else if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid]+1)
+            left=mid+1;
+            else right=mid-1;
         }
         return -1;
     }
