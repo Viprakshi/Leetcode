8d50af6bd15d2edac7e7cee48bc2c289d26698b5
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-02 15:15:43 +0530

    Time: 0 ms (100%), Space: 26.1 MB (93.88%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        
-        if(nums.size()==1)return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int left=1, right=n-2;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if(mid%2==0){ //even
-                if(nums[mid]==nums[mid+1]){
-                    //even,odd --> eliminate left
-                    left=mid+1;
-                }
-                else{
-                    //odd,even ---> eliminate right
-                    right=mid-1;
-                }
-            } 
-            else{
-                //mid is odd
-                if(nums[mid]==nums[mid+1]){
-                    //odd, even --> eliminate right
-                    right=mid-1;
-                }
-                else{
-                    //even, odd ---> eliminate left
-                    left=mid+1;
-                }
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int left = 1, right = n - 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+            if (nums[mid] == nums[mid - 1]) {
+                // Pair starts at mid-1
+                if ((mid - 1) % 2 == 0)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            else { // nums[mid] == nums[mid + 1]
+                // Pair starts at mid
+                if (mid % 2 == 0)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
         }
+
         return -1;
     }
 };
