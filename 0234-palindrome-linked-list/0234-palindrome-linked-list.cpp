af430c2dd0b0a317574411418c24ca3e1bb43413
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-03 22:48:34 +0530

    Time: 1 ms (67.88%), Space: 118.1 MB (53.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,37 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
-
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (fast->next && fast->next->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+        if(head==NULL || head->next==NULL) return true;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast && fast->next && fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
 
-        ListNode *prev = nullptr;
-        ListNode *curr = slow->next;
-
-        while (curr) {
-            ListNode *nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
+        ListNode *curr=slow->next;
+        slow->next = NULL;  
+        ListNode *prev=slow;
+        while(curr){
+            ListNode *nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
         }
-
-        ListNode *first = head;
-        ListNode *second = prev;
-
-        while (second) {
-            if (first->val != second->val)
-                return false;
-
-            first = first->next;
-            second = second->next;
+        ListNode *last=prev;
+        ListNode *first=head;
+        while(last && first){
+            if(last->val!=first->val) return false;
+            last=last->next;
+            first=first->next;
         }
-
         return true;
     }
 };
