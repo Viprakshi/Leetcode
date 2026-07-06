987005bf405e71385ca2894a37650eb86012d1ab
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-06 15:47:13 +0530

    Time: 2 ms (58.87%), Space: 18.3 MB (20.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void generate(vector<int>& arr, int target,vector<vector<int>>& ans, vector<int>& temp, int pos,int sum){
-        if(sum==target){
-            ans.push_back(temp);
+    void gen(vector<int>& arr, int target, vector<vector<int>>& ans, int curr_sum,int curr_ind, vector<int>& curr_ans){
+        if(curr_sum>target || curr_ind==arr.size()){
+            if(curr_sum==target){
+                ans.push_back(curr_ans);
+            }
             return;
         }
-        if(sum>target || pos>=arr.size()) return;
-        generate(arr,target,ans,temp,pos+1,sum);
-        temp.push_back(arr[pos]);
-        generate(arr,target,ans,temp,pos,sum+arr[pos]);
-        temp.pop_back();
+        gen(arr,target,ans,curr_sum,curr_ind+1,curr_ans);
+        curr_ans.push_back(arr[curr_ind]);
+        gen(arr,target,ans,curr_sum+arr[curr_ind],curr_ind,curr_ans);
+        curr_ans.pop_back();
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        generate(candidates, target, ans,temp,0,0);
+        vector<int> curr;
+        gen(candidates, target, ans,0,0,curr);
         return ans;
     }
 };
