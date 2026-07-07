3460ec6011cc76607ea77a232a189c8f239e4e84
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-08 00:03:09 +0530

    Time: 7 ms (63.72%), Space: 11.8 MB (65.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,13 @@ public:
         unordered_map<char,int> mpp;
         int i=0,j=0, ans=0;
         while(j<s.length()){
-            if(mpp[s[j]]!=0){
-                while(mpp[s[j]]!=0){
-                    mpp[s[i]]--;
-                    i++;
-                }
+            mpp[s[j]]++;
+            while(mpp[s[j]]>1){
+                mpp[s[i]]--;
+                i++;
             }
-            else{
-                ans=max(ans, j-i+1);
-                mpp[s[j]]++;
-                j++;
-            }
+            ans=max(ans, j-i+1);
+            j++;
         }
         return ans;
     }
