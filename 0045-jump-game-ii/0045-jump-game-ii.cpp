187c820a7c0225b73a97eff415c371ad4ffacd1d
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-09 16:37:35 +0530

    Time: 0 ms (100%), Space: 20.6 MB (43.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    int minJumps(vector<int>& nums, int position, vector<int>& dp) {
-
-        if(position >= nums.size()-1) return 0;
-        if(nums[position] == 0) return INT_MAX;
-
-        if(dp[position] != -1)
-            return dp[position];
-
-        int minStep = INT_MAX;
-
-        for(int jump=1; jump<=nums[position]; jump++) {
-
-            int subResult =
-                minJumps(nums, position+jump, dp);
-
-            if(subResult != INT_MAX)
-                minStep = min(minStep, 1 + subResult);
-        }
-
-        return dp[position] = minStep;
-    }
-
     int jump(vector<int>& nums) {
-
-        vector<int> dp(nums.size(), -1);
-
-        return minJumps(nums, 0, dp);
+        int jumps=0; 
+        int max_dis=0;
+        int last=0;
+        for(int i=0;i<nums.size()-1;i++){
+            max_dis=max(max_dis, i+nums[i]);
+            if(i==last){
+                jumps++;
+                last=max_dis;
+            }
+        }
+        return jumps;
     }
 };
