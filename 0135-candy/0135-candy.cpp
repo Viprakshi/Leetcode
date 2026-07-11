d4972ea7941242470619474bbd3ce06371b767ba
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-11 23:01:40 +0530

    Time: 0 ms (100%), Space: 22.9 MB (52.23%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> ans(n, 1);
-
-        // Handle up slopes
-        for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i - 1]) {
-                ans[i] = ans[i - 1] + 1;
-            }
+        int ans=0;
+        vector<int> r(ratings.size(),1);
+        for(int i=1;i<ratings.size();i++){
+            if(ratings[i]>ratings[i-1]) r[i]=r[i-1]+1;
         }
-
-        // Handle down slopes
-        for(int i = n - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i + 1]) {
-                ans[i] = max(ans[i], ans[i + 1] + 1);
-            }
+        for(int i=ratings.size()-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) r[i]=max(r[i],r[i+1]+1);
         }
-
-        int total = 0;
-        for(int x : ans) total += x;
-
-        return total;
+        for(int x:r) ans+=x;
+        return ans;
     }
 };
