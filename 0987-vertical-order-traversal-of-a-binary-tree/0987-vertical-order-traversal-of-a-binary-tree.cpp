966eff06d1e01eb6659635e1a486dab1304f6467
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-13 15:17:16 +0530

    Time: 7 ms (5.15%), Space: 16.5 MB (31.55%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,34 +12,28 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mpp;
-        queue<pair<TreeNode*,pair<int, int>>> q;
-        q.push({root, {0,0}});
-        while(!q.empty()){
-            TreeNode* curr=q.front().first;
-            int vertical=q.front().second.first;
-            int horizontal=q.front().second.second;
+       map<int,map<int,multiset<int>>> mpp;
+       queue<pair<TreeNode*,pair<int,int>>> q;
+       q.push({root,{0,0}});
+       while(!q.empty()){
+            auto curr=q.front();
             q.pop();
-            mpp[vertical][horizontal].insert(curr->val);
-            if(curr->left)
-            q.push({curr->left,{vertical-1, horizontal+1}});
-            if(curr->right)
-            q.push({curr->right,{vertical+1, horizontal+1}});
-            
-
-            
-        }
-        vector<vector<int>> ans;
-         for (auto p : mpp) {
-            vector<int> col;
-            // Collect all nodes in order of levels
-            for (auto q : p.second) {
+            int x=curr.second.first;
+            int y=curr.second.second;
+            mpp[x][y].insert(curr.first->val);
+            if(curr.first->left)
+            q.push({curr.first->left,{x-1,y+1}});
+            if(curr.first->right)
+            q.push({curr.first->right,{x+1,y+1}});
+       }
+       vector<vector<int>> ans;
+       for(auto curr:mpp){
+        vector<int> col;
+            for (auto q : curr.second) {
                 col.insert(col.end(), q.second.begin(), q.second.end());
             }
-            // Push the column into result
             ans.push_back(col);
-        }
-
-        return ans;
+       }
+       return ans;
     }
 };
