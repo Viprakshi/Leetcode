c95d0a9f2ed403d0ac7655455bdb612802ca6704
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-15 22:58:53 +0530

    Time: 4 ms (53.08%), Space: 32.1 MB (15.78%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -12,31 +12,28 @@
 class BSTIterator {
 public:
     stack<TreeNode*> st;
-
-    void pushAllLeft(TreeNode* node) {
-        while (node) {
-            st.push(node);
-            node = node->left;
-        }
-    }
-
     BSTIterator(TreeNode* root) {
-        pushAllLeft(root);
+        pushall(st, root);
     }
-
+    
     int next() {
-        TreeNode* node = st.top();
-        st.pop();
-
-        if (node->right) {
-            pushAllLeft(node->right);
-        }
-
-        return node->val;
+       TreeNode* top=st.top();
+       st.pop();
+       if(top->right){
+        pushall(st, top->right);
+       }
+        return top->val;
     }
-
+    
     bool hasNext() {
-        return !st.empty();
+        if(st.empty()) return false;
+        return true;
+    }
+    void pushall(stack<TreeNode*>& st, TreeNode* root){
+        while(root){
+            st.push(root);
+            root=root->left;
+        }
     }
 };
 
