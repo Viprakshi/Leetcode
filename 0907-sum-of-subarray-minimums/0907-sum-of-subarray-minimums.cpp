8d9470afad5b3c8453a47256574ccae8501f9de3
Viprakshi Gupta <165376147+Viprakshi@users.noreply.github.com>
2026-07-16 18:13:13 +0530

    Time: 75 ms (38.18%), Space: 193.3 MB (58.87%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        long long ans = 0;
         int n = arr.size();
-        const int MOD = 1e9 + 7;
-
+        vector<int> left(n), right(n);
         stack<int> st;
+        int mod = 1e9+7;
 
-        for (int i = 0; i <= n; i++) {
-            while (!st.empty() && (i == n || arr[st.top()] >= arr[i])) {
-                int mid = st.top();
+        // Previous Smaller Element
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i])
                 st.pop();
 
-                int left = st.empty() ? -1 : st.top();
-                int right = i;
+            if(st.empty()) left[i]=-1;
+            else left[i]=st.top();
 
-                ans += 1LL * arr[mid] * (mid - left) * (right - mid);
-                ans %= MOD;
-            }
             st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        // Next Smaller or Equal Element
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i])
+                st.pop();
+
+            if(st.empty()) right[i]=n;
+            else right[i]=st.top();
+
+            st.push(i);
+        }
+
+        long long ans=0;
+
+        for(int i=0;i<n;i++){
+            long long leftCount=i-left[i];
+            long long rightCount=right[i]-i;
+            ans=(ans+1LL*arr[i]*leftCount*rightCount)%mod;
         }
 
         return ans;
